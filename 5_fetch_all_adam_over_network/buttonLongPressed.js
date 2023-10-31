bd7a9d88d60e646525b2087d11db63217d4b7577
jatin kumar kamboj <kumar.jatin873@gmail.com>
2023-10-31 15:09:56 +0530

    code structuring using try and catch

--- 5_fetch_all_adam_over_network/buttonLongPressed.js
+++ 5_fetch_all_adam_over_network/buttonLongPressed.js
@@ -1,48 +1,53 @@
-// to check whether the button has been pressed for long time
+// to check whether the button has been pressed for long time say more than 5 seconds
 
 const ModbusRTU = require('modbus-serial')
 const client = new ModbusRTU()
 
-client.connectTCP("192.168.0.206", { port: 502 }, () => console.log('ADAM Device Connected'))
+client.connectTCP("192.168.0.206", { port: 502 }, () => console.log('ADAM Device Connected at 192.168.0.206...!!'))
 client.setID(1)
 
 let inPutAddress = 0, buttonPressedTime = null, buttonReleasedTime = null
 
 async function readButtonState() {
-    // let inPutAddress = 0, buttonPressedTime = null, buttonReleasedTime = null
-    let readInputResponse = await client.readDiscreteInputs(inPutAddress, 1)
-    console.log('readInputResponse :- ', readInputResponse)
-    let buttonState = readInputResponse.data[0]
-    console.log('current State :- ', buttonState)
+    try {
+        // let inPutAddress = 0, buttonPressedTime = null, buttonReleasedTime = null
+        let readInputResponse = await client.readDiscreteInputs(inPutAddress, 1)
+        console.log('readInputResponse :- ', readInputResponse)
+        let buttonState = readInputResponse.data[0] // getting false as an input response
+        console.log('current State :- ', buttonState) // will print "false" as an input response
 
-    if (!buttonState) {
-        console.log('Button Pressed')
-        if ((buttonPressedTime == null) && !buttonState) {
-            buttonPressedTime = new Date()
+        if (!buttonState) {
+            console.log('Button Pressed')
+            if ((buttonPressedTime == null) && !buttonState) {
+                buttonPressedTime = new Date()
+            }
+        } else if (buttonPressedTime != null) {
+            console.log('Button Released')
+            if ((buttonReleasedTime == null) && buttonState) {
+                buttonReleasedTime = new Date()
+            }
         }
-    } else if (buttonPressedTime != null) {
-        console.log('Button Released')
-        if ((buttonReleasedTime == null) && buttonState) {
-            buttonReleasedTime = new Date()
-        }
-    }
 
-    console.log(`buttonPressedTime :- ${buttonPressedTime} and buttonReleasedTime :- ${buttonReleasedTime}`)
+        console.log(`buttonPressedTime :- ${buttonPressedTime} and buttonReleasedTime :- ${buttonReleasedTime}`)
 
-    if ((buttonPressedTime != null) && (buttonReleasedTime != null)) {
-        const buttonPressDuration = Math.floor((buttonReleasedTime - buttonPressedTime)/1000)
-        console.log('buttonPressDuration :- ', buttonPressDuration)
+        if ((buttonPressedTime != null) && (buttonReleasedTime != null)) {
+            const buttonPressDuration = Math.floor((buttonReleasedTime - buttonPressedTime)/1000)
+            console.log('buttonPressDuration :- ', buttonPressDuration)
 
-        const buttonLongPressThreshold = 5 // in seconds
+            const buttonLongPressThreshold = 5 // in seconds
 
-        // wait for 5 seconds if button released or not
-        if (buttonPressDuration >= buttonLongPressThreshold) {
-            console.log('Long Press Detected....Button has been pressed for 5 seconds or more than 5 seconds')
+            // wait for 5 seconds if button released or not
+            if (buttonPressDuration >= buttonLongPressThreshold) {
+                console.log('Long Press Detected....Button has been pressed for 5 seconds or more than 5 seconds')
+            } else {
+                console.log('Button has been released before 5 seconds')
+            }
         } else {
-            console.log('Button has been released before 5 seconds')
+            console.log('Button has not been pressed/released yet...')
         }
-    } else {
-        console.log('Button has not been pressed/released yet...')
+    } catch (error) {
+        console.log('Facing an Error :- ', error)
+        process.exit(1)
     }
 }
 
