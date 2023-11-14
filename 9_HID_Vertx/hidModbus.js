38073992b842935359006f8e25683471122b06be
jatin kumar kamboj <kumar.jatin873@gmail.com>
2023-11-14 14:26:18 +0530

    config content added

--- 9_HID_Vertx/hidModbus.js
+++ 9_HID_Vertx/hidModbus.js
@@ -3,13 +3,18 @@ const ModBusSerial = require("modbus-serial")
 // Create a Modbus client instance
 const client = new ModBusSerial()
 
+const config = {
+    host: "192.168.0.204",
+    port: 4050
+}
+
 // connection_port (4070) TCP outbound. This port must be open on the Host computer.
 // listen_port (4050) TCP incoming. This port must be open when using selected applications. 
-client.connectTCP("192.168.0.204", { port: 4050 }, (err) => { // --> ip for Vertx controller
+client.connectTCP(config.host, { port: config.port }, (err) => { // --> ip for Vertx controller
     if (err) {
         console.log('Error while connecting the HID Device', err)
     } else {
-        console.log('HID device connected at 192.168.0.204')
+        console.log(`HID Device Connected at ${config.host}:${config.port}`)
     }
 })
 client.setID(1)
