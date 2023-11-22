8d0ab0559226dcacc86679e8da8bab3af4336f74
Jatin Kamboj <kumar.jatini873@gmail.com>
2023-11-22 16:58:14 +0530

    hid in progress

--- 10_hid_dll_test/server.js
+++ 10_hid_dll_test/server.js
@@ -1,8 +1,21 @@
 // for using ffi-napi npm module we may need python version 3.11.0
-const ffi = require('ffi-napi');
+const ffi = require('ffi-napi')
+const path = require('path')
 
-const myLibrary = ffi.Library('./VertXDLL_2[1].4.7_R22716/Defines.h', {
-    hereiamStruct: ['string', ['string']]
-})
+async function serverTest() {
+    console.log('Testing == === == ===')
+    try {
+        const vertXDefineLibPath = path.join(__dirname, 'VertXDLL_2[1].4.7_R22716', 'Vertx.dll')
+        console.log('vertXDefineLibPath :- ', vertXDefineLibPath)
 
-myLibrary.hereiamStruct(3)
+        const vertXLib = ffi.Library(vertXDefineLibPath, {
+            GetDLLVersion: ['int', ['charPointer', 'int']]
+        })
+        let outPut = vertXLib.GetDLLVersion()
+        console.log('outPut :- ', outPut)
+    } catch (error) {
+        console.log('Error :- ', error)
+    }
+}
+
+serverTest()
