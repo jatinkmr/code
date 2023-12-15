b4213cd5fe5a859dba8a93384dbde864f88ba97d
Jatin Kamboj <kumar.jatini873@gmail.com>
2023-12-15 16:02:55 +0530

    msg done

--- 18_node_kafka_video_api/controller.js
+++ 18_node_kafka_video_api/controller.js
@@ -3,18 +3,40 @@ const { KafkaConfig } = require('./config')
 const sendVideoFromDrive = async (req, res) => {
     try {
         // this below code is sending and receiving the req.body.message from the server(s) consume function of KafkaJS
+        // const { message } = req.body
+        // const kafkaConfig = new KafkaConfig()
+
+        // let messages = [{
+        //     key: "key1",
+        //     value: message
+        // }]
+        // console.log(`messages :- ${JSON.stringify(messages)}, message :- ${message}`);
+
+        // kafkaConfig.produce('my-vid', messages)
+
+        // this below code is sending and receiving the video(s) buffer from the producer to consumer which have data length more than 1MB (viz Default size)
         const { message } = req.body
-        const kafkaConfig = new KafkaConfig()
+        const kafaconfig = new KafkaConfig()
 
-        let messages = [{
-            key: "key1",
-            value: message
-        }]
-        console.log(`messages :- ${JSON.stringify(messages)}, message :- ${message}`);
+        if (Array.isArray(message)) { // if message is an array
+            for (let msg of message) {
+                let messageData = [{
+                    key: 'frame_1',
+                    value: msg
+                }]
 
-        kafkaConfig.produce('my-vid', messages)
+                await kafaconfig.produce('my-vid', messageData)
+            }
+        } else { // if message is just a string
+            let messageData = [{
+                key: 'frame_1',
+                value: message
+            }]
+            await kafaconfig.produce('my-vid', messageData)
+        }
 
         res.status(200).json({
+            messageDataLength: message.length,
             data: message
         })
     } catch (error) {
